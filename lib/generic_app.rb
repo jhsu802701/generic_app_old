c13d059b1b83a627f5032df4fa9c2e3c16b94765
Vagrant User <vagrant@vagrant-rvm>
2015-01-21 23:04:53 -0600

    Created executable

--- lib/generic_app.rb
+++ lib/generic_app.rb
@@ -1,5 +1,12 @@
 require "generic_app/version"
 
 module GenericApp
-  # Your code goes here...
+  def self.create
+    puts "DEFAULT VALUE: sample_app_3rd_edition"
+    puts "Enter the directory name you wish to use for your generic app:"
+    subdir_name = gets.chomp
+    system("git clone https://github.com/mhartl/sample_app_3rd_edition.git #{subdir_name}")
+    system("cd #{subdir_name} && git checkout remotes/origin/account-activation-password-reset")
+    system ("cd #{subdir_name} && git init")
+  end
 end
