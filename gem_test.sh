cc5e78ed50498c8469b25887c723e3a3745765a5
Vagrant User <vagrant@vagrant-rvm>
2015-03-27 16:46:45 -0500

    Separated components of starting Rails app into several functions; added logging of test results

--- gem_test.sh
+++ gem_test.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
 
 gem uninstall generic_app
+DIR_GENERIC_APP=$PWD
+mkdir -p log
+
 echo "**************"
 echo "bundle install"
 bin/setup >/dev/null
 echo "*************************"
 echo "BEGIN TESTING generic_app"
-rake
+rake 2>&1 | tee $DIR_GENERIC_APP/log/generic_app.txt
 echo "FINISHED TESTING generic_app"
 echo "****************************"
 
 echo "********************************"
 echo "BEGIN TESTING THE tmp1 RAILS APP"
-cd tmp1 && sh test.sh
+cd tmp1 && sh test.sh 2>&1 | tee $DIR_GENERIC_APP/log/tmp1.txt
 echo "FINISHED TESTING THE tmp1 RAILS APP"
 echo "***********************************"
 
 echo "********************************"
 echo "BEGIN TESTING THE tmp2 RAILS APP"
 cd ..
-cd tmp2 && sh test.sh
+cd tmp2 && sh test.sh 2>&1 | tee $DIR_GENERIC_APP/log/tmp2.txt
 echo "FINISHED TESTING THE tmp2 RAILS APP"
 echo "***********************************"
-
-echo "If all went well, there are no error messages and no failed tests."
+echo "The log of the generic_app test is at $DIR_GENERIC_APP/log/generic_app.txt."
+echo "The log of the tmp1 Rails app test is at $DIR_GENERIC_APP/log/tmp1.txt."
+echo "The log of the tmp2 Rails app test is at $DIR_GENERIC_APP/log/tmp2.txt."
+echo
+echo "If all went well, the results at the end of each test show 0 or 31m0 failures and 0 errors."
 echo
--- lib/generic_app.rb
+++ lib/generic_app.rb
@@ -4,55 +4,60 @@ require "generic_app/version"
 require "string_in_file"
 require "line_containing"
 
+ENV['DIR_MAIN'] = File.expand_path("../../", __FILE__)
+
 module GenericApp
 
   # Create app, stick with SQLite database in development
   def self.sq (subdir_name)
-    ENV['DIR_MAIN'] = File.expand_path("../../", __FILE__)
-    subdir_name_final = subdir_name
-    subdir_name_final = "sample_app_3rd_edition" if subdir_name == ""
     t1 = Thread.new { 
-      puts "*************************************************"
-      puts "Downloading the Sample App from railstutorial.org"
-      system("git clone https://github.com/mhartl/sample_app_3rd_edition.git #{subdir_name}")
-      system("cd #{subdir_name} && git checkout remotes/origin/account-activation-password-reset")
-      }
-    t1.join
-    t1 = Thread.new { 
-      puts "*************************************************************"
-      puts "Setting the Guardfile to automatically run tests upon startup"
-      str_guard_orig = "all_on_start: false"
-      str_guard_new = "all_on_start: true"
-      StringInFile.replace(str_guard_orig, str_guard_new, "#{subdir_name}/Guardfile")
-      
-      puts "*********************************************************************************"
-      puts "Advising users to use a password management program to create and store passwords"
-      str1 = "</h1>"
-      str2a = "</h1>"
-      str2b = "\nUsing the same password for all of your accounts is risky."
-      str2b += "\nLimiting yourself to passwords that you can easily remember is risky."
-      str2b += "\nYou should use a password management program like <a href='http://www.keepassx.org/'>KeePassX</a>"
-      str2b += "\nto create much better passwords AND store them in encrypted form.<br>"
-      str2 = str2a + str2b
-      StringInFile.replace(str1, str2, "#{subdir_name}/app/views/users/new.html.erb")
-      StringInFile.replace(str1, str2, "#{subdir_name}/app/views/users/edit.html.erb")
-      StringInFile.replace(str1, str2, "#{subdir_name}/app/views/password_resets/new.html.erb")
-      StringInFile.replace(str1, str2, "#{subdir_name}/app/views/password_resets/edit.html.erb")
-
-      puts "*************************************"
-      puts "Adding notes and routine task scripts"
-      system("cp -r #{ENV['DIR_MAIN']}/to_add/* #{subdir_name}")
-      system("cd #{subdir_name} && sh list_files.sh")
-      
-      puts "********************************************"
-      puts "Adding tmp* and .DS_Store to .gitignore file"
-      open("#{subdir_name}/.gitignore", 'a') { |f|
-        f << "\n\n"
-        f << "\ntmp*"
-        f << "\n.DS_Store"
-      }
+      self.git_clone (subdir_name)
     }
     t1.join
+    self.guard_file (subdir_name)
+    self.app_views_password (subdir_name)
+    self.guard_file (subdir_name)
+    self.app_views_password (subdir_name)
+    self.git_init (subdir_name)
+    self.add_scripts (subdir_name)
+    self.add_notes (subdir_name)
+    self.add_readme (subdir_name)
+    t1 = Thread.new { 
+      self.update_gitignore (subdir_name)
+    }
+    t1.join
+  end
+  
+  def self.git_clone (subdir_name)
+    puts "*************************************************"
+    puts "Downloading the Sample App from railstutorial.org"
+    system("git clone https://github.com/mhartl/sample_app_3rd_edition.git #{subdir_name}")
+    system("cd #{subdir_name} && git checkout remotes/origin/account-activation-password-reset")
+
+  end
+  
+  def self.guard_file (subdir_name)
+    puts "*************************************************************"
+    puts "Setting the Guardfile to automatically run tests upon startup"
+    str_guard_orig = "all_on_start: false"
+    str_guard_new = "all_on_start: true"
+    StringInFile.replace(str_guard_orig, str_guard_new, "#{subdir_name}/Guardfile")
+  end
+  
+  def self.app_views_password (subdir_name)
+    puts "*********************************************************************************"
+    puts "Advising users to use a password management program to create and store passwords"
+    str1 = "</h1>"
+    str2a = "</h1>"
+    str2b = "\nUsing the same password for all of your accounts is risky."
+    str2b += "\nLimiting yourself to passwords that you can easily remember is risky."
+    str2b += "\nYou should use a password management program like <a href='http://www.keepassx.org/'>KeePassX</a>"
+    str2b += "\nto create much better passwords AND store them in encrypted form.<br>"
+    str2 = str2a + str2b
+    StringInFile.replace(str1, str2, "#{subdir_name}/app/views/users/new.html.erb")
+    StringInFile.replace(str1, str2, "#{subdir_name}/app/views/users/edit.html.erb")
+    StringInFile.replace(str1, str2, "#{subdir_name}/app/views/password_resets/new.html.erb")
+    StringInFile.replace(str1, str2, "#{subdir_name}/app/views/password_resets/edit.html.erb")
   end
   
   def self.git_init (subdir_name)
@@ -64,6 +69,41 @@ module GenericApp
     system("cd #{subdir_name} && git add .")
     system("cd #{subdir_name} && git commit -m 'Initial commit' >/dev/null")
     $stdout = STDOUT
+  end
+  
+  def self.add_scripts (subdir_name)
+    puts "*******************"
+    puts "Adding Bash scripts"
+    system("cp -r #{ENV['DIR_MAIN']}/to_add/*.sh #{subdir_name}")
+    system("cd #{subdir_name} && sh list_files.sh")
+  end
+  
+  def self.add_notes (subdir_name)
+    puts "*****************************"
+    puts "Adding notes on MVC structure"
+    system("mkdir -p #{subdir_name}/notes")
+    system("cp -r #{ENV['DIR_MAIN']}/to_add/notes/* #{subdir_name}/notes")
+  end
+  
+  def self.add_readme (subdir_name)
+    puts "*****************************"
+    puts "Adding README"
+    system("cp -r #{ENV['DIR_MAIN']}/to_add/README.md #{subdir_name}")
+  end
+  
+  def self.update_gitignore (subdir_name)
+    puts "*******************"
+    puts "Updating .gitignore"
+    if StringInFile.present("tmp*","#{subdir_name}/.gitignore") == false
+      command = 'echo "tmp*" >> '
+      command += "#{subdir_name}/.gitignore"
+      system(command)
+    end
+    if StringInFile.present(".DS_Store","#{subdir_name}/.gitignore") == false
+      command = 'echo ".DS_Store" >> '
+      command += "#{subdir_name}/.gitignore"
+      system(command)
+    end
   end
   
   # Create app, use PostgreSQL instead of SQLite
@@ -78,10 +118,16 @@ module GenericApp
   # Username
   # Password
 
-
   def self.pg (subdir_name, db_rootname_x, var_store_username, var_store_password, username_x, password_x)
     self.sq (subdir_name)
-    
+    self.pg_gemfile (subdir_name)
+    t1 = Thread.new { 
+      self.set_pg_params(subdir_name, db_rootname_x, var_store_username, var_store_password, username_x, password_x)
+    }
+    t1.join
+  end
+  
+  def self.pg_gemfile (subdir_name)
     puts "**************************************************************************"
     puts "Updating the Gemfile (PostgreSQL for development, testing, and production)"
     LineContaining.delete("sqlite", "#{subdir_name}/Gemfile")
@@ -90,10 +136,6 @@ module GenericApp
       f << "\n\ngem 'pg'\n"
       f << "gem 'figaro'\n"
     }
-    t1 = Thread.new { 
-      self.set_pg_params(subdir_name, db_rootname_x, var_store_username, var_store_password, username_x, password_x)
-    }
-    t1.join
   end
   
   def self.set_pg_params (subdir_name, db_rootname_x, var_store_username, var_store_password, username_x, password_x)
