de974066e10d3ed4979bd72365d71037878c27ae
Vagrant User <vagrant@vagrant-rvm>
2015-07-14 03:17:54 -0500

    Changed gem_install.sh due to errors

--- gem_install.sh
+++ gem_install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-gem uninstall generic_app
+gem cleanup generic_app
 bin/setup
-echo "****************************"
-echo "BEGIN INSTALLING generic_app"
-rake install
+echo '------------------------'
+echo 'Building generic_app gem'
+gem build generic_app.gemspec
+echo '------------------------'
+echo 'Installing generic_app gem'
+gem install generic_app*.gem
 echo "FINISHED INSTALLING generic_app"
 echo "*******************************"
