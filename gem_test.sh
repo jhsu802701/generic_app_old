b2607cea744a97b04fd6dd345696e2ce0fdd3853
Jason Hsu <jhsu802701@jasonhsu.com>
2017-01-24 22:48:25 +0000

    Shortened gem_test.sh

--- gem_test.sh
+++ gem_test.sh
@@ -11,20 +11,13 @@ bin/setup >/dev/null
 
 echo '=================='
 echo 'BEGIN PART 1: rake'
-rake 2>&1 | tee $DIR_GENERIC_APP/log/1.txt
+rake
 echo 'FINISHED PART 1: rake'
 echo '====================='
 echo 
-echo '====================='
-echo 'BEGIN PART 2: rubocop'
-rubocop 2>&1 | tee $DIR_GENERIC_APP/log/2.txt
-echo 'FINISHED PART 2: rubocop'
 echo '========================'
+echo 'BEGIN PART 2: rubocop -D'
+rubocop -D
+echo 'FINISHED PART 2: rubocop -D'
+echo '==========================='
 echo
-echo '==================================='
-echo 'The locations of the test logs are:'
-echo "PART 1: $DIR_GENERIC_APP/log/1.txt"
-echo "PART 2: $DIR_GENERIC_APP/log/2.txt"
-
-echo
-echo 'If all went well, all tests passed with no offenses.'
