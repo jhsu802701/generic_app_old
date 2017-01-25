e20cd6e9905e4fd1b8cdccec46bfbd328e7e7064
Jason Hsu <jhsu802701@jasonhsu.com>
2017-01-25 20:27:59 +0000

    Removed rubocop from gem_test.sh; covered in code_test.sh and all.sh

--- gem_test.sh
+++ gem_test.sh
@@ -9,15 +9,6 @@ echo '=============='
 echo 'bundle install'
 bin/setup >/dev/null
 
-echo '=================='
-echo 'BEGIN PART 1: rake'
+echo '===='
+echo 'rake'
 rake
-echo 'FINISHED PART 1: rake'
-echo '====================='
-echo 
-echo '========================'
-echo 'BEGIN PART 2: rubocop -D'
-rubocop -D
-echo 'FINISHED PART 2: rubocop -D'
-echo '==========================='
-echo
